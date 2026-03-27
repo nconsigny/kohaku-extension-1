468064945a11794f372d99a8201f80452be00183
nconsigny <nicolas@ethereum.org>
2026-03-27 17:22:26 +0100

    fix: exclude WASM files from Metro file map

--- metro.config.js
+++ metro.config.js
@@ -5,4 +5,7 @@ const config = getDefaultConfig(__dirname)
 
 config.transformer.assetPlugins = ['expo-asset/tools/hashAssetFiles']
 
+// Exclude WASM files from Metro's file map (they're handled by webpack)
+config.resolver.blockList = [/\.wasm$/]
+
 module.exports = config
