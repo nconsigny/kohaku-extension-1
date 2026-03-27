23502c1bf36b60e57179ff01f56a8a10c22599b7
nconsigny <nicolas@ethereum.org>
2026-03-27 17:17:26 +0100

    fix: use correct WASM init function name

--- src/web/libs/eth-prices/eth_prices.js
+++ src/web/libs/eth-prices/eth_prices.js
@@ -1,9 +1,9 @@
 /* @ts-self-types="./eth_prices.d.ts" */
 
 import wasm from "./eth_prices_bg.wasm";
-import { __wbg_set_wasm, __wbindgen_start } from "./eth_prices_bg.js";
+import { __wbg_set_wasm, __wbindgen_init_externref_table } from "./eth_prices_bg.js";
 __wbg_set_wasm(wasm);
-if (typeof __wbindgen_start === 'function') __wbindgen_start();
+__wbindgen_init_externref_table();
 export {
     Quoter, Route, createQuoter
 } from "./eth_prices_bg.js";
